4554238a7b758c3ded2d63e5fa227ef55f6fcdb4
jampttws <tanasorn42@hotmail.com>
2018-03-16 15:22:01 +0700

    updated

--- src/counter/ObserverTest.java
+++ src/counter/ObserverTest.java
@@ -3,15 +3,19 @@ package counter;
 import java.util.Scanner;
 
 public class ObserverTest {
-	 Scanner console = new Scanner(System.in);
-     Counter counter = new Counter();  // The observable subject
-     ConsoleView view = new ConsoleView(counter);
-//     counter.addObserver(view);
-//     while(true) {
-//         System.out.print("Count how many? ");
-//         int howmany = console.nextInt();
-//         if (howmany==0) System.exit(0);
-//         counter.add(howmany);
-//}
-
+	
+	public static void main(String[] args){
+		Scanner console = new Scanner(System.in);
+	     Counter counter = new Counter();  // The observable subject
+	     ConsoleView view = new ConsoleView(counter);
+	     counter.addObserver(view);
+	     
+	     while(true){
+	         System.out.print("Count how many? ");
+	         int howmany = console.nextInt();
+	         if (howmany==0) System.exit(0);
+	         counter.add(howmany);
+	     }
+	}
+	
 }
